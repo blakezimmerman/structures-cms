153f5d4c5d16ae37f3fcaf534ba0afc7d52d4e5f
Blake Zimmerman <blake.zimmerman@icloud.com>
2017-08-27 00:15:52 -0400

    Fix routing with webpack builds

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,7 +8,7 @@ export const checkMatchFound = (x: any, res: express.Response) =>
     ? res.status(500).json('Could not find a matching structure')
     : res.json(x);
 
-const ApiRoutes = (app: any) => {
+const ApiRoutes = (app: express.Express) => {
   app.use("/api/structures", structuresRoutes);
   app.use("/api/entries", entriesRoutes);
   app.use("/api/users", usersRoutes);
--- src/server/webpackBuild.ts
+++ src/server/webpackBuild.ts
@@ -1,9 +1,12 @@
+import * as express from 'express';
+import * as path from 'path';
+
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackConfig = require('../../webpack/webpack.dev');
 const compiler = webpack(webpackConfig);
 
-const webpackBuild = (app: any) => {
+const webpackBuild = (app: express.Express) => {
   app.use(webpackDevMiddleware(compiler, {
     historyApiFallback: true,
     stats: {
@@ -12,7 +15,20 @@ const webpackBuild = (app: any) => {
     watchOptions: {
       ignored: /node_modules/
     },
+    publicPath: '/'
   }));
+
+  app.use('/*', (req, res, next) => {
+    var filename = path.join(compiler.outputPath, 'index.html');
+    compiler.outputFileSystem.readFile(filename, (err: any, result: any) => {
+      if (err) {
+        return next(err);
+      }
+      res.set('content-type','text/html');
+      res.send(result);
+      res.end();
+    });
+  });
 }
 
 export default webpackBuild;
